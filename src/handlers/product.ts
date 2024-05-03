0794c2ada7ab3a7872aa620f6d83dd3f9f15f108
Said_Nouasria <saidnouasria23@gmail.com>
2024-05-03 14:11:25 +0100

    update handlers

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -44,7 +44,7 @@ export const updateProduct = async (req, res) => {
       id_belongsToId: {
         id: req.params.id,
         belongsToId: req.user.id,
-      },
+      }
     },
     data: {
       name: req.body.name,
--- src/handlers/update.ts
+++ src/handlers/update.ts
@@ -52,25 +52,25 @@ export const createUpdate = async (req, res) => {
 };
 
 export const updateUpdate = async (req, res) => {
-   const products= await prisma.product.findMany({
-    where:{
-      belongsToId:req.user.id
-    },
-    include : {
-        Update:true
-    }
-   })
+  //  const products= await prisma.product.findMany({
+  //   where:{
+  //     belongsToId:req.user.id
+  //   },
+  //   include : {
+  //       Update:true
+  //   }
+  //  })
 
-     const updates = products.reduce((allUpdates,product)=>{
-      return[...allUpdates,...product.Update]
-     },[])
+  //    const updates = products.reduce((allUpdates,product)=>{
+  //     return[...allUpdates,...product.Update]
+  //    },[])
 
 
-     const match = updates.find((update) => update.id === req.params.id);
+  //    const match = updates.find((update) => update.id === req.params.id);
      
-     if (!match){
-      return res.json({message :"nope"})
-     }
+  //    if (!match){
+  //     return res.json({message :"nope"})
+  //    }
 
   const updated = await prisma.product.update({
     where: {
@@ -84,28 +84,11 @@ export const updateUpdate = async (req, res) => {
 
 export const deleteUpdate = async (req, res) => {
 
-    const products = await prisma.product.findMany({
-      where: {
-        belongsToId: req.user.id,
-      },
-      include: {
-        Update: true,
-      },
-    });
-
-    const updates = products.reduce((allUpdates, product) => {
-      return [...allUpdates, ...product.Update];
-    }, []);
-
-    const match = updates.find((update) => update.id === req.params.id);
-
-    if (!match) {
-      return res.json({ message: "nope" });
-    }
 
   const deleted = await prisma.update.delete({
     where: {
         id: req.params.id,
+
     },
   });
   res.json({ data: deleted });
